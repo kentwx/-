558acad0a1fea9c38e43b50b0b88ac07a82c81c9
jim87 <jim87@LAPTOP-53B7AMOE>
2020-07-11 16:06:40 +0800

    12321

---
+++ 期末報告/index.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class _index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session.IsNewSession == true)
+        {
+            Application.Lock();
+            Session["Visited"] = true;
+            StreamReader dr = new StreamReader(Server.MapPath("Counter.txt"));
+            Application["Counter"] = int.Parse(dr.ReadLine());
+            dr.Close();
+            Application["Counter"] = int.Parse(Application["Counter"].ToString()) + 1;
+            StreamWriter wr = new StreamWriter(Server.MapPath("Counter.txt"));
+            wr.WriteLine(Application["Counter"]);
+            wr.Close();
+            Application.UnLock();
+        }
+        lblCounter.Text = "您是本站第" + Application["Counter"].ToString() + "位的訪客";
+    }
+}
